63f259cb4b8c2e0070a0d333f28114f000d1cff6
milly1110ning-dot <milly.1110ning@gmail.com>
2026-03-13 12:07:08 +0800

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Test
+{
+	private int Mid;
+	private int Final;
+
+	public double GetAvg()
+	{
+		return (Mid+Final)/2.0;
+	}
+
+	public void SetGrade(int m,int f)
+	{
+		Mid = m;
+		Final = f;
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Class_0313_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Test joe, mary;
+
+            joe= new Test();
+            mary= new Test();
+
+            joe.SetGrade(70, 80);
+            mary.SetGrade(70, 90);
+
+
+            Console.WriteLine(joe.GetAvg());
+
+            Console.WriteLine(mary.GetAvg());
+
+        }
+    }
+}
